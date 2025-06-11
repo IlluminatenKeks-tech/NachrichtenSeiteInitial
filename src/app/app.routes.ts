f8db5a5dc41420eff84d2d83f4466325b8a5e6d2
Antonia <antonia.wiedenmann@web.de>
2025-06-11 13:42:40 +0200

    Erster Stand

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {JokesComponent} from './jokes/jokes.component';
+import {NewsComponent} from './news/news.component';
+import {TodayComponent} from './today/today.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'jokes', component: JokesComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'today', component: TodayComponent },
+];
