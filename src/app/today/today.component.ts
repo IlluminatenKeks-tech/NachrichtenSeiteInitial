9ccd0000907224147e341deac781b85482bc18f5
Antonia <antonia.wiedenmann@web.de>
2025-06-12 13:27:40 +0200

    Alle aktuellen News aus der Welt und aus Deutschland werden angezeigt -- Bilder funktionieren leider nicht

--- src/app/today/today.component.ts
+++ src/app/today/today.component.ts
@@ -9,11 +9,27 @@ import {DataService} from '../shared/data.service';
 })
 export class TodayComponent {
 news: any;
+globalNews: any;
+globalNewsTeaserPictures: any [] = [];
+germanyNews: any;
+germanyNewsTeaserPictures : any [] = [];
+
+
 
 constructor(private dataService: DataService) { }
   async ngOnInit() {
   this.news = await this.dataService.getNews();
-  this.news = this.news.news;
+  this.germanyNews = this.news.regional
+    for(let i = 0; i < this.germanyNews.length; i++){
+      this.germanyNewsTeaserPictures[i] = this.germanyNews[i].teaserImage;
+    }
+    this.globalNews = this.news.news;
+    for(let i = 0; i < this.globalNews.length; i++){
+      this.globalNewsTeaserPictures[i] = this.globalNews[i].teaserImage;
+    }
+
+  console.log(this.germanyNews);
   console.log(this.news);
+  console.log(this.germanyNewsTeaserPictures);
   }
 }
